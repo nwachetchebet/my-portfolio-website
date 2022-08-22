f3d3e25c0fced0b8048b8b98f2ec4c6ebc54a6ca
Tonie <victornwanochi@gmail.com>
2022-08-22 23:42:45 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,181 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close'),
+    mainModal = document.getElementById('main-modal')
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+        mainModal.classList.add('nav-menu-container')
+    })
+}
+
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu'),
+        mainModal.classList.remove('nav-menu-container')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction () {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show menu class
+    navMenu.classList.remove('show-menu'),
+    mainModal.classList.remove('nav-menu-container')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+  });
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+//========FORM SUBMISSION====================
+const form = document.getElementById("my-form"),
+        modalView = document.querySelector('.services__modal'),
+        modalBtns = document.querySelector('.submit__btn'),
+        modalClose =document.querySelector('.services__modal-close')
+        
+    
+async function handleSubmit(event) {
+    event.preventDefault();
+
+   const modalParagraph = document.querySelector('.modal_paragraph')
+   const modalHeader = document.querySelector('.modal_header')
+    //var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+    }).then(response => {
+    if (response.ok) {
+        modalView.classList.add('active-modal'),
+        setTimeout(() => { 
+            modalView.classList.remove('active-modal')
+        }, 4000);
+        form.reset()
+    } else {
+        response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+            modalView.classList.add('active-modal')
+            modalParagraph.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            //tatus.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            modalHeader.innerHTML = " "
+        } else {
+           // status.innerHTML = "Oops! There was a problem submitting your form"
+           modalView.classList.add('active-modal')
+            modalParagraph.innerHTML = "There has been a problem submtting your form, Please try again"
+            modalHeader.innerHTML = " OOPS"
+        }
+        })
+    }
+    }).catch(error => {
+    modalParagraph.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
+modalClose.addEventListener('click', () => {
+         modalView.classList.remove('active-modal')
+     });
